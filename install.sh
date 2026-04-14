fdc4dc51388d3a25a2594b1f9cb98bec4aba80dc
mwtheus <mwtheus.sh@gmail.com>
2026-04-14 15:59:45 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
 epoch() { date +%s; }
 
-mkdir "$HOME/.bkp_dotfiles"
+mkdir -p "$HOME/.bkp_dotfiles"
+
 for item in .*; do
-  if [[ "$item" != '.git' && "$item" != '.' && "$item" != '..' ]]; then
-    mv $HOME/$item $HOME/.bkp_dotfiles/$item\_$(epoch)
-    ln -sf ./$item $HOME
+  if [[ "$item" != '.' && "$item" != '..' && "$item" != '.git' ]]; then
+
+    target="$HOME/$item"
+    source="$(pwd)/$item"
+
+    if [ -e "$target" ] || [ -L "$target" ]; then
+      mv "$target" "$HOME/.bkp_dotfiles/${item}_$(epoch)" 2>/dev/null || true
+    fi
+
+    ln -sf "$source" "$target"
   fi
 done
-
-bash
